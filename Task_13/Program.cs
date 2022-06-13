a0b96cdf3b1ae06377e92ba47d10940330c39204
Sergey <metrolog_omsk@mail.ru>
2022-06-13 11:23:29 +0500

    Решил задачу № 13 при помощи массивов

---
+++ Task_13/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("****************************************");
+Console.WriteLine("Введите произвольное число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N / 100 == 0) // Проверяем наличие третьего числа
+{
+    Console.WriteLine($"У числа {N} нет третьей цифры");
+}
+else
+{
+    int count = 0; 
+    int number = N;
+    while (number > 0) // Находим количество символов числа для заполнения массива
+    {
+        number = number / 10;
+        count++;
+    }
+
+    int[] array = new int[count]; // Заполняем массив
+    int length = array.Length;
+    int index = 0;
+    int del = N;
+
+    while (index < length)
+    {
+        array[length - 1 - index] = del % 10;
+        del = del / 10;
+        index++;
+    }  
+
+Console.Write($"Третья цифра числа {N} - это {array[2]}");
+Console.WriteLine();
+}
+Console.WriteLine("****************************************");
