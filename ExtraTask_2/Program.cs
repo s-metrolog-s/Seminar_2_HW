520f6c731c95472e3bd95cb2457e43952a5a0df6
Sergey <metrolog_omsk@mail.ru>
2022-06-15 07:01:53 +0500

    Начало решения доп. задачи № 2

--- ExtraTask_2/Program.cs
+++ ExtraTask_2/Program.cs
@@ -1,2 +1,18 @@
 ﻿// Задача 2. На вход подаются год, номер месяца и день рождения человека.
 // Определить возраст человека на момент 1 июля 2022 года.
+
+Console.WriteLine("Введите день, месяц и год рождения в формате dd:mm:yyyy:");
+
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day < 1 && day > 31)
+{
+    day = Convert.ToInt32(Console.ReadLine());
+}
+
+int month = Convert.ToInt32(Console.ReadLine());
+int year = Convert.ToInt32(Console.ReadLine());
+
+
+
+Console.WriteLine($"{day}:{month}:{year}");
