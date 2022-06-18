34b32013a8e797c6940f4c172c95614ad5c5dac6
Sergey <metrolog_omsk@mail.ru>
2022-06-18 08:20:53 +0500

    Решил дополнительную задачу № 2

--- ExtraTask_2/Program.cs
+++ ExtraTask_2/Program.cs
@@ -1,18 +1,40 @@
 ﻿// Задача 2. На вход подаются год, номер месяца и день рождения человека.
 // Определить возраст человека на момент 1 июля 2022 года.
 
-Console.WriteLine("Введите день, месяц и год рождения в формате dd:mm:yyyy:");
+Console.Clear();
+Console.WriteLine("*********************************************************");
 
-int day = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+int day = rand.Next(1, 32);
+int month = rand.Next(1, 13);
+int year = rand.Next(1980, 2022);
+string [] nameMonths = {"января", "февраля", "марта", 
+                    "апреля", "марта", "июня", 
+                    "июля", "августа", "сентября", 
+                    "октября", "ноября", "декабря"};
 
-if (day < 1 && day > 31)
+Console.WriteLine($"Случайный день рождения человека {day} {nameMonths[month-1]} {year}");
+Console.WriteLine("Рассчитываем возраст на 01 июля 2022");
+
+int age = 2022 - year;
+if (month < 7)
 {
-    day = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Возраст человека составляет {age} года/лет");
+}
+else if (month > 7)
+{
+    Console.WriteLine($"Возраст человека составляет {age - 1} года/лет");
+}
+else if (month == 7)
+{
+    if (day == 1)
+    {
+        Console.WriteLine($"Возраст человека составляет {age} года/лет, с днем рождения!");
+    }
+    else
+    {
+        Console.WriteLine($"Возраст человека составляет {age - 1} года/лет");
+    }
 }
 
-int month = Convert.ToInt32(Console.ReadLine());
-int year = Convert.ToInt32(Console.ReadLine());
-
-
-
-Console.WriteLine($"{day}:{month}:{year}");
+Console.WriteLine("*********************************************************");
